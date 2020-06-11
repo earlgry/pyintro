a8829067f24fccf428c77a07a396390eaa6637b9
unknown <anything@anything.com>
2020-06-11 04:52:28 -0400

    new stuff

--- hello.py
+++ hello.py
@@ -1,9 +1,18 @@
-name = input();
-
-
-
-print("HELLO WORLD")
+brianbprint("HELLO WORLD")
 print("Dont think twice...that's all right")
+print("WRITE A NAME: ")
 
-
+name = input();
 print(f"hello, {name}!")
+
+i = 28
+print(f"i is {i}")
+
+f= 2.8
+print(f"f is {f}")
+
+b = True
+print(f"b is {b}")
+
+n = None
+print(f"n is {n}")
