b9eb6ca855ed41869d511dbb6882d1631794224e
Muhammad Ahmad <farooqahmadmuhammad@gmail.com>
2020-06-12 01:43:01 -0400

    new file update

--- first.py
+++ first.py
@@ -1,4 +1,19 @@
 print("Hello")
+print("what is your name")
 
 name = input()
 print(f"hello, {name}!")
+
+#variable called none which stores a none type: does not store any kind of values
+#python has 4 times of values..integers, floating, boolean, none
+#in python you dont need to specify value type
+
+x = 28
+
+print(f"{x}")
+if x>0:
+    print("x is positive")
+elif x<0:
+    print("x is negative")
+else:
+    print("x is zero")
